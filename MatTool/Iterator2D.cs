1dbccc51be302864089419340a2225e97dbbd0ec
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-12-16 22:26:50 +0700

    iterator 2d

--- MatTool/Iterator2D.cs
+++ MatTool/Iterator2D.cs
@@ -1,7 +1,60 @@
-﻿
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using static System.Math;
+
 namespace FingerprintRecognitionV2.MatTool
 {
-    public class Iterator2D
+    static public class Iterator2D
     {
+        /** 
+         * @ core
+         * */
+        static public void Forward(int t, int l, int d, int r, Func<int, int, bool> f)
+        {
+            for (int y = t; y < d; y++)
+                for (int x = l; x < r; x++)
+                    f(y, x);
+        }
+
+        static public void Forward(int h, int w, Func<int, int, bool> f)
+        {
+            Forward(0, 0, h, w, f);
+        }
+
+        /** 
+         * @ 2d-array
+         * */
+        static public void Forward<T>(T[,] mat, Func<int, int, bool> f)
+        {
+            Forward(0, 0, mat.GetLength(0), mat.GetLength(1), f);
+        }
+
+        static public void Forward<T>(T[,] mat, int t, int l, int d, int r, Func<int, int, bool> f)
+        {
+            t = Max(0, t);
+            l = Max(0, l);
+            d = Min(mat.GetLength(0), d);
+            r = Min(mat.GetLength(1), r);
+            Forward(t, l, d, r, f);
+        }
+
+        /** 
+         * @ emgu image
+         * */
+        static public void Forward<T>(Image<Gray, T> img, Func<int, int, bool> f)
+            where T : new()
+        {
+            Forward(img.Height, img.Width, f);
+        }
+
+        static public void Forward<T>(Image<Gray, T> img, int t, int l, int d, int r, Func<int, int, bool> f)
+            where T : new()
+        {
+            t = Max(0, t);
+            l = Max(0, l);
+            d = Min(img.Height, d);
+            r = Min(img.Width, r);
+            Forward(t, l, d, r, f);
+        }
     }
 }
