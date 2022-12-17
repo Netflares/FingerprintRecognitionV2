1aeded7a7c7108b1dc16e86b6563152a52fa5d36
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-12-18 00:43:40 +0700

    i'll leave this for tomorrow

---
+++ MatTool/MatWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+
+namespace FingerprintRecognitionV2.MatTool
+{
+    public class MatWrapper<T>
+        where T : new()
+    {
+        public T[,] Arr;
+
+        /** 
+         * @ constructors
+         * */
+        protected MatWrapper()
+        {
+            Arr = new T[0, 0];
+        }
+
+        /// <summary>
+        /// Perform a SHALLOW COPY of `T[,] src`. Any modification in this wrapper will be written directly to `src`
+        /// </summary>
+        public MatWrapper(T[,] src)
+        {
+            Arr = src;
+        }
+
+        /** 
+         * @ dynamic operators
+         * 
+         * assume that two matrices in a binary operator have the same size
+         * */
+        public void And(bool[,] b)
+        {
+            for (int y = 0; y < b.GetLength(0); y++)
+                for (int x = 0; x < b.GetLength(1); x++)
+                    if (!b[y, x]) Arr[y, x] = new();
+        }
+
+        /** 
+         * @ static operators
+         * 
+         * assume that two matrices in a binary operator have the same size
+         * */
+        static public MatWrapper<T> operator &(MatWrapper<T> a, bool[,] b)
+        {
+            MatWrapper<T> res = new(a.Arr.Clone() as T[,]);
+            res.And(b);
+            return res;
+        }
+    }
+}
---
+++ MatTool/NumMatWrapper.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Numerics;
+
+namespace FingerprintRecognitionV2.MatTool
+{
+    public class NumMatWrapper<T> : MatWrapper<T>
+        where T : INumber<T>, new()
+    {
+        /// <summary>
+        /// Perform a SHALLOW COPY of `T[,] src`. Any modification in this wrapper will be written directly to `src`
+        /// </summary>
+        public NumMatWrapper(T[,] src)
+        {
+            Arr = src;
+        }
+
+        /** 
+         * @ dynamic operators between matrices
+         * 
+         * assume that two matrices in a binary operator have the same size
+         * */
+        public void Mul<TOther>(TOther[,] v)
+            where TOther : INumber<TOther>, new()
+        {
+            Merge(v, (a, b) => a * b);
+        }
+
+        public void Merge<TOther>(TOther[,] v, Func<T, T, T> f)
+            where TOther : INumber<TOther>, new()
+        {
+            for (int y = 0; y < Arr.GetLength(0); y++)
+                for (int x = 0; x < Arr.GetLength(1); x++)
+                    Arr[y, x] = f(Arr[y, x], (T)(object)v[y, x]);
+        }
+
+        /** 
+         * @ dynamic operators with a real value
+         * */
+        public void Mul<TOther>(TOther v)
+            where TOther : INumber<TOther>, new()
+        {
+            Merge(v, (a, b) => a * b);
+        }
+
+        public void Merge<TOther>(TOther v, Func<T, T, T> f)
+            where TOther : INumber<TOther>, new()
+        {
+            for (int y = 0; y < Arr.GetLength(0); y++)
+                for (int x = 0; x < Arr.GetLength(1); x++)
+                    Arr[y, x] = f(Arr[y, x], (T)(object) v);
+        }
+
+        /** 
+         * @ static operators between matrices
+         * */
+        static public NumMatWrapper<T> operator +(NumMatWrapper<T> a, T[,] b)
+        {
+            NumMatWrapper<T> res = new(a.Arr.Clone() as T[,]);
+            res.Merge(b, (a, b) => a + b);
+            return res;
+        }
+
+        static public NumMatWrapper<T> operator +(NumMatWrapper<T> a, NumMatWrapper<T> b)
+        {
+            return a + b.Arr;
+        }
+    }
+}
