1419466b73d0d431b54e7e5a03af9672a42d8e59
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-12-17 13:17:00 +0700

    f c#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FingerprintRecognitionV2.DataStructure;
+
+NumPair<int, int> a = new(20, 20);
+NumPair<int, int> b = new(10, 5);
+a += b;
+Console.WriteLine(a);
