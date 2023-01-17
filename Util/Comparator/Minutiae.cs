dd8629a95490f6cbca0eef112dac1b872d05191d
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-01-17 17:03:46 +0700

    imma party first

--- Util/Comparator/Minutiae.cs
+++ Util/Comparator/Minutiae.cs
@@ -3,14 +3,14 @@ namespace FingerprintRecognitionV2.Util.Comparator
 {
     public class Minutiae
     {
-        static public readonly int NO_TYPE = 0, ENDING = 1, BIFUR = 2;
+        static public readonly byte NO_TYPE = 0, ENDING = 1, BIFUR = 2;
 
-        public double T;    // type
+        public byte T;      // type
         public double Y;    // y location
         public double X;    // x location
         public double A;    // the angle
 
-        public Minutiae(int t, double y, double x, double a)
+        public Minutiae(byte t, double y, double x, double a)
         {
             T = t;
             Y = y;
--- Util/Preprocessing/MinutiaeExtractor.cs
+++ Util/Preprocessing/MinutiaeExtractor.cs
@@ -20,30 +20,40 @@ namespace FingerprintRecognitionV2.Util.Preprocessing
         static public List<Minutiae> Extract(bool[,] ske, double[,] orient, bool[,] msk, int wl, int bs)
         {
             int h = ske.GetLength(0), w = ske.GetLength(1);
-            List<Minutiae> res = new();
-            bool[,] visited = new bool[h, w];
+            bool[,] vst = new bool[h, w];
+            byte[,] mat = new byte[h, w];
 
             Iterator2D.Forward(1, 1, h - 1, w - 1, (y, x) =>
             {
-                if (msk[y, x] && !visited[y, x])
+                if (msk[y, x] && !vst[y, x])
                 {
-                    int t = CheckMinutiae(ske, y, x);
+                    byte t = CheckMinutiae(ske, y, x);
                     if (t == Minutiae.NO_TYPE) return;
 
-                    res.Add(new(t, y, x, orient[y / bs, x / bs]));
+                    mat[y, x] = t;
                     // won't accept more minutiae from this area
                     Iterator2D.Forward(
                         // should have t = y - wl,
                         // but since we won't come back to that area again so...
-                        visited, y, x - wl, y + wl, x + wl, (r, c) => visited[r, c] = true
+                        vst, y, x - wl, y + wl, x + wl, (r, c) => vst[r, c] = true
                     );
                 }
+            });
+
+            ClearNoise(mat, h, w, wl);
+            List<Minutiae> res = new();
+
+            Iterator2D.Forward(mat, (y, x) =>
+            {
+                if (mat[y, x] != Minutiae.NO_TYPE) res.Add(
+                    new(mat[y, x], y, x, orient[y / bs, x / bs])
+                );
             });
 
             return res;
         }
 
-        static private int CheckMinutiae(bool[,] img, int y, int x)
+        static private byte CheckMinutiae(bool[,] img, int y, int x)
         {
             if (!img[y, x]) return Minutiae.NO_TYPE;
             int n = CountTransitions(img, y, x);
@@ -72,9 +82,58 @@ namespace FingerprintRecognitionV2.Util.Preprocessing
             return CountTrue(!p2 && p3, !p3 && p4, !p4 && p5, !p5 && p6, !p6 && p7, !p7 && p8, !p8 && p9, !p9 && p2);
         }
 
-        private static int CountTrue(params bool[] args)
+        static private int CountTrue(params bool[] args)
         {
             return args.Count(t => t);
+        }
+
+        /** 
+         * @ usage:
+         * clear noises
+         * */
+        static private void ClearNoise(byte[,] src, int h, int w, int wl)
+        {
+            bool[,] msk = new bool[h, w];
+            int[,] pre = CreatePrefix(src, h, w);
+
+            Iterator2D.Forward(wl, wl, h - wl, w - wl, (y, x) =>
+            {
+
+            });
+
+            MorphologyR4.Close(msk, 8);
+            Iterator2D.PForward(msk, (y, x) =>
+            {
+                if (msk[y, x]) pre[y, x] = Minutiae.NO_TYPE;
+            });
+        }
+
+        /**
+         * @ result:
+         * res[y, x] = sum(src[0:y, 0:x])
+         * 
+         * @ note:
+         * this method is optimized for this class only
+         * to use this for general purposes, uncomment some lines below
+         * */
+        static private int[,] CreatePrefix(byte[,] src, int h, int w)
+        {
+            int[,] res = new int[h, w];
+
+            // // can skip this because there's no minutiae in this edge
+            // for (int y = 0; y < h; y++) if (src[y, 0] != Minutiae.NO_TYPE) res[y, 0] = 1;
+            // for (int x = 1; x < w; x++) if (src[0, x] != Minutiae.NO_TYPE) res[0, x] = 1;
+
+            for (int y = 1; y < h; y++)
+            {
+                for (int x = 1; x < w; x++)
+                {
+                    res[y, x] = res[y - 1, x] + res[y, x - 1];
+                    if (src[y, x] != Minutiae.NO_TYPE) res[y, x]++;
+                }
+            }
+
+            return res;
         }
     }
 }
