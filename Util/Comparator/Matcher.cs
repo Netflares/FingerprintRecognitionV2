ffa63e10291e52c037c5a274911d2065182e0db8
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-02-17 13:36:46 +0700

    matcher update

--- Util/Comparator/Matcher.cs
+++ Util/Comparator/Matcher.cs
@@ -34,61 +34,22 @@ namespace FingerprintRecognitionV2.Util.Comparator
 		}
 
 		/** 
-		 * @ init
+		 * @ public
 		 * */
-
 		List<TripletPair> mTriplets = new();
 		List<MinutiaPair> mPairs = new();
 		FastHashSet<int> probeDupes = new();
 		FastHashSet<int> candidateDupes = new();
+		List<Minutia> probeMatch = new();
+		List<Minutia> candidateMatch = new();
 
-		public Matcher() 
-		{
+		public Matcher() {}
 
-		}
-
-		/** 
-		 * @ the core 
-		 * */
 		// returns the number of matching minutiae
 		public int Match(Fingerprint probe, Fingerprint candidate)
 		{
-			mTriplets.Clear();
-			mPairs.Clear();
-			probeDupes.Clear();
-			candidateDupes.Clear();
-
-			List<Triplet> probeT = probe.Triplets, candidateT = candidate.Triplets;
-
-			// 5.1.2
-			foreach (Triplet p in probeT)
-			{
-				int l = candidate.LowerBound(p.Distances[2] - Param.LocalDistanceTolerance);
-				if (l == candidateT.Count) continue;
-				int r = candidate.LowerBound(p.Distances[2] + Param.LocalDistanceTolerance);
-
-				for (; l < r; l++)
-				{
-					double score = Similarity.sPart(p, candidateT[l]);
-					if (score > 0) mTriplets.Add(new(p, candidateT[l], score));
-				}
-			}
-
-			// 5.1.3
-			mTriplets.Sort((a, b) => a.Score > b.Score ? -1 : 1);	// descending
-
-			// 5.1.5
-			foreach (TripletPair p in mTriplets)
-			{
-				for (int i = 0; i < 3; i++)
-				{
-					Minutia a = p.Probe.Minutiae[i], b = p.Candidate.Minutiae[i];
-					int aKey = a.Y << 10 | a.X,
-						bKey = b.Y << 10 | b.X;
-					if (probeDupes.Add(aKey) || candidateDupes.Add(bKey))
-						mPairs.Add(new(a, b));
-				}
-			}
+			if (!Init(probe, candidate)) return 0;
+			LocalMatching(probe, candidate);
 
 			// global matching
 			int ans = 0;
@@ -97,6 +58,7 @@ namespace FingerprintRecognitionV2.Util.Comparator
 			{
 				Minutia m1 = pair1.Probe, m2 = pair1.Candidate;
 				
+				// a little caching
 				double theta = m2.Angle - m1.Angle,
 					   cosTheta = Math.Cos(theta), 
 					   sinTheta = Math.Sin(theta);
@@ -108,39 +70,80 @@ namespace FingerprintRecognitionV2.Util.Comparator
 					if (pair1.Equals(pair2)) continue;
 					Minutia m3 = pair2.Probe, m4 = pair2.Candidate;
 
-					if (Geometry.AdPI(
-						Geometry.Ad2PI(m2.Angle, m1.Angle), Geometry.Ad2PI(m4.Angle, m3.Angle)
-					) > Param.AngleTolerance) continue;
-
-					// finding p'
-					double x = m2.X + cosTheta * (m3.X - m1.X) - sinTheta * (m3.Y - m1.Y),
-						   y = m2.Y + sinTheta * (m3.X - m1.X) + cosTheta * (m3.Y - m1.Y),
-						   t = m3.Angle - m1.Angle + m2.Angle;
-					double edgeA = x - m4.X, edgeB = y - m4.Y, edgeC = edgeA * edgeA + edgeB * edgeB;
-					
-					if (edgeC > Param.GlobalDistanceTolerance * Param.GlobalDistanceTolerance) continue;
-					if (Geometry.AdPI(t, m4.Angle) > Param.AngleTolerance) continue;
-
-					matches++;
+					if (CheckM4(m1, m2, m3, m4, sinTheta, cosTheta))
+						matches++;
 				}
 
-				if (matches > ans) ans = matches;
+				ans = Math.Max(ans, matches);
+			}
+
+			return ans;
+		}
+
+		public int Match(Fingerprint probe, Fingerprint candidate, ref List<Minutia> mProbe, ref List<Minutia> mCandidate)
+		{
+			if (!Init(probe, candidate)) return 0;
+			LocalMatching(probe, candidate);
+
+			// global matching
+			int ans = 0;
+
+			foreach (MinutiaPair pair1 in mPairs)
+			{
+				Minutia m1 = pair1.Probe, m2 = pair1.Candidate;
+
+				probeMatch.Clear();
+				candidateMatch.Clear();
+				probeMatch.Add(m1);
+				candidateMatch.Add(m2);
+				
+				// a little caching
+				double theta = m2.Angle - m1.Angle,
+					   cosTheta = Math.Cos(theta), 
+					   sinTheta = Math.Sin(theta);
+
+				int matches = 0;
+
+				foreach (MinutiaPair pair2 in mPairs)
+				{
+					if (pair1.Equals(pair2)) continue;
+					Minutia m3 = pair2.Probe, m4 = pair2.Candidate;
+
+					if (!CheckM4(m1, m2, m3, m4, sinTheta, cosTheta))
+						continue;
+
+					matches++;
+					probeMatch.Add(m3);
+					candidateMatch.Add(m4);
+				}
+
+				if (matches > ans) {
+					ans = matches;
+					mProbe = probeMatch;
+					mCandidate = candidateMatch;
+				}
 			}
 
 			return ans;
 		}
 
 		/** 
-		 * @ debuggers 
+		 * @ 
 		 * */
-		// a cheap copy-pasting, but it works regardless
-		public int DebugMatch(Fingerprint probe, Fingerprint candidate, ref List<Minutia> ansProbe, ref List<Minutia> ansCandidate)
+		private bool Init(Fingerprint probe, Fingerprint candidate)
 		{
 			mTriplets.Clear();
 			mPairs.Clear();
 			probeDupes.Clear();
 			candidateDupes.Clear();
 
+			// check whether these 2 fingerprints satisfy the contraints
+
+			return true;
+		}
+
+		private void LocalMatching(Fingerprint probe, Fingerprint candidate)
+		{
 			List<Triplet> probeT = probe.Triplets, candidateT = candidate.Triplets;
 
 			// 5.1.2
@@ -172,55 +175,26 @@ namespace FingerprintRecognitionV2.Util.Comparator
 						mPairs.Add(new(a, b));
 				}
 			}
+		}
 
-			// global matching
-			int ans = 0;
+		private bool CheckM4(Minutia m1, Minutia m2, Minutia m3, Minutia m4, double sinTheta, double cosTheta)
+		{
+			// 5.2.2
+			if (Geometry.AdPI(
+				Geometry.Ad2PI(m2.Angle, m1.Angle), Geometry.Ad2PI(m4.Angle, m3.Angle)
+			) > Param.AngleTolerance) return false;
 
-			foreach (MinutiaPair pair1 in mPairs)
-			{
-				Minutia m1 = pair1.Probe, m2 = pair1.Candidate;
-				
-				double theta = m2.Angle - m1.Angle,
-					   cosTheta = Math.Cos(theta), 
-					   sinTheta = Math.Sin(theta);
+			// finding p'
+			double x = m2.X + cosTheta * (m3.X - m1.X) - sinTheta * (m3.Y - m1.Y),
+				   y = m2.Y + sinTheta * (m3.X - m1.X) + cosTheta * (m3.Y - m1.Y),
+				   t = m3.Angle - m1.Angle + m2.Angle;
+			double edgeA = x - m4.X, edgeB = y - m4.Y, edgeC = edgeA * edgeA + edgeB * edgeB;
+			
+			if (edgeC > Param.GlobalDistanceTolerance * Param.GlobalDistanceTolerance) return false;
+			if (Geometry.AdPI(t, m4.Angle) > Param.AngleTolerance) return false;
 
-				int matches = 0;
-				List<Minutia> mProbe = new(), mCandidate = new();
-				mProbe.Add(m1);
-				mCandidate.Add(m2);
-
-				foreach (MinutiaPair pair2 in mPairs)
-				{
-					if (pair1.Equals(pair2)) continue;
-					Minutia m3 = pair2.Probe, m4 = pair2.Candidate;
-
-					if (Geometry.AdPI(
-						Geometry.Ad2PI(m2.Angle, m1.Angle), Geometry.Ad2PI(m4.Angle, m3.Angle)
-					) > Param.AngleTolerance) continue;
-
-					// finding p'
-					double x = m2.X + cosTheta * (m3.X - m1.X) - sinTheta * (m3.Y - m1.Y),
-						   y = m2.Y + sinTheta * (m3.X - m1.X) + cosTheta * (m3.Y - m1.Y),
-						   t = m3.Angle - m1.Angle + m2.Angle;
-					double edgeA = x - m4.X, edgeB = y - m4.Y, edgeC = edgeA * edgeA + edgeB * edgeB;
-					
-					if (edgeC > Param.GlobalDistanceTolerance * Param.GlobalDistanceTolerance) continue;
-					if (Geometry.AdPI(t, m4.Angle) > Param.AngleTolerance) continue;
-
-					matches++;
-					mProbe.Add(m3);
-					mCandidate.Add(m4);
-				}
-
-				if (matches > ans)
-				{
-					ans = matches;
-					ansProbe = mProbe;
-					ansCandidate = mCandidate;
-				}
-			}
-
-			return ans;
+			// maybe check sth else here before returning
+			return true;
 		}
 	}
 }
