ed9562d4e40ceb25a9cde34d84fc4529a40d2330
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-02-21 14:54:28 +0700

    1-n comparison

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ using FingerprintRecognitionV2.Util.Comparator;
 using FingerprintRecognitionV2.Util.FeaturesExtractor;
 using FingerprintRecognitionV2.Util.Preprocessing;
 using System.Diagnostics;
+using System.IO;
 
 class Program
 {
@@ -44,19 +45,28 @@ class Program
                 break;
 
             case "match-11":
-                // program match-11 img-1 img-2 ans-path
+                // program match-11 img1-path img2-path ans-path
                 if (a.Length == 4)
                     MatchOneOne(a[1], a[2], a[3]);
-                // program match-11 img-1 img-2 dat-1 dat-2 ans-path
-                else if (a.Length == 6)
-                    MatchOneOne(a[1], a[2], a[3], a[4], a[5]);
                 else
                     PrintHelp();
                 break;
 
             case "match-1n":
-                // program match-1n
-                PrintHelp();
+                // program match-1n thread-cnt probe-path candidate-dir ans-path
+                
+                if (a.Length == 5) 
+                {
+                    int threads = 1;
+                    try 
+                    { 
+                        threads = Convert.ToInt32(a[1]); 
+                    } 
+                    catch (Exception e) { PrintHelp(); break; }
+                    MatchOneMany(threads, a[2], a[3], a[4]);
+                }
+                else
+                    PrintHelp();
                 break;
 
             case "match-nn":
@@ -91,7 +101,7 @@ class Program
      * */
 
     /**
-     * @ matching section
+     * @ matching section - One to One
      * */
     static void MatchOneOne(string fProbe, string fCandi, string fAns)
     {
@@ -100,22 +110,63 @@ class Program
         Fingerprint f = ProcFingerprint(proc, fProbe);
         Fingerprint g = ProcFingerprint(proc, fCandi);
 
+        MatchOneOne(fProbe, fCandi, f, g, fAns);
+    }
+
+    static void MatchOneOne(string fProbe, string fCandi, Fingerprint probe, Fingerprint candi, string fAns)
+    {
         // match
         List<Minutia> mf = new(), mg = new();
         Matcher matcher = new();
-        int mCnt = matcher.Match(f, g, ref mf, ref mg);
+        int mCnt = matcher.Match(probe, candi, ref mf, ref mg);
 
         // return
-        Console.WriteLine("matches: " + mCnt);
         Image<Bgr, byte> ans = Visualization.VisualizeComparison(fProbe, fCandi, ImgHeight, ImgWidth, mf, mg);
         CvInvoke.Imwrite(fAns, ans);
     }
 
-    static void MatchOneOne(string fProbe, string fCandi, string fProbeDat, string fCandiDat, string fAns)
+    /**
+     * @ matching section - One to Many
+     * */
+    static void MatchOneMany(int threads, string fProbe, string dCandi, string fAns)
     {
-        Console.WriteLine("To be implemented");
+        // process probe
+        Processor proc = new();
+        Fingerprint probe = ProcFingerprint(proc, fProbe);
+
+        // get candidates
+        string[] fCandidates = Directory.GetFiles(dCandi);
+        int[] score = new int[fCandidates.Length];
+
+        // allocate threads
+        int threadSize = (fCandidates.Length + threads - 1) / threads;   // ceil(a / b)
+        Parallel.For(0, threads, (t) =>
+        {
+            int l = t * threadSize, r = Math.Min((t + 1) * threadSize, fCandidates.Length);
+            Processor proc = new();
+            Matcher mtch = new();
+            for (; l < r; l++) 
+            {
+                Fingerprint candidate = ProcFingerprint(proc, fCandidates[l]);
+                score[l] = mtch.Match(probe, candidate);
+            }
+        });
+
+        // get best match
+        int ansInd = 0, ansScore = -1;
+        for (int i = 0; i < score.Length; i++) if (score[i] >= ansScore) 
+        {
+            ansInd = i; ansScore = score[i];
+        }
+
+        // return
+        Fingerprint ans = ProcFingerprint(proc, fCandidates[ansInd]);
+        MatchOneOne(fProbe, fCandidates[ansInd], probe, ans, fAns);
     }
 
+    /** 
+     * @ tools 
+     * */
     static Fingerprint ProcFingerprint(Processor proc, string f)
     {
         proc.Process(new Image<Gray, byte>(f));
@@ -124,9 +175,6 @@ class Program
         return new(dat);
     }
 
-    /** 
-     * @ tools 
-     * */
     static void Debug()
     {
 
