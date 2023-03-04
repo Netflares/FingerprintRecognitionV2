dfd96d48e53d9398c85f199355d48e21416b6c1d
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-03-04 17:06:52 +0700

    additional rules for skeletonization

--- Util/Preprocessing/ZhangBruteThinning.cs
+++ Util/Preprocessing/ZhangBruteThinning.cs
@@ -3,9 +3,15 @@ namespace FingerprintRecognitionV2.Util.Preprocessing
 {
 	static public class ZhangBruteThinning
 	{
+        /**
+         * @ consts
+         * */
         static readonly int Height = Param.Height, Width = Param.Width;
         static readonly int MaxIterations = 7;
 
+        /**
+         * @ zhang-suen
+         * */
         static public void Thinning(bool[,] src)
         {
             var tmp = (bool[,])src.Clone();
@@ -20,6 +26,10 @@ namespace FingerprintRecognitionV2.Util.Preprocessing
                 iterations--;
             }
             while (cnt > 0 && iterations > 0);
+            // apply the third rules set
+            for (int y = 1; y < Height - 1; y++)
+                for (int x = 1; x < Width - 1; x++)
+                    src[y, x] = src[y, x] && Clean(src, y, x);
         }
 
         static private int Step(bool step, bool[,] tmp, bool[,] src)
@@ -69,6 +79,39 @@ namespace FingerprintRecognitionV2.Util.Preprocessing
             return false;
         }
 
+        /**
+         * @ additional skeleton enhancement
+         * 
+         * the skeleton image built by Zhang-Suen algorithm is, unfortunately,
+         * imperfect
+         * 
+         * thereof, these methods are written
+         * to remove unwanted pattern from the skeleton
+         * */
+        static public bool Clean(bool[,] src, int y, int x) 
+        {
+            // take p1 - a 1 cell
+            // decide whether it would continue be a 1 cell
+
+            var p2 = src[y - 1, x];
+            var p3 = src[y - 1, x + 1];
+            var p4 = src[y, x + 1];
+            var p5 = src[y + 1, x + 1];
+            var p6 = src[y + 1, x];
+            var p7 = src[y + 1, x - 1];
+            var p8 = src[y, x - 1];
+            var p9 = src[y - 1, x - 1];
+
+            if (p9 && p8 && p6 && !p3) return false;
+            if (p3 && p4 && p6 && !p9) return false;
+            if (p5 && p6 && p8 && !p3) return false;
+            if (p4 && p6 && p7 && !p9) return false;
+            return true;
+        }
+
+        /**
+         * @ tools
+         * */
         private static int CountTrue(params bool[] args)
         {
             return args.Count(t => t);
